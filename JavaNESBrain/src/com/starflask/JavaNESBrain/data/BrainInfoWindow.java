5eb81a9d1694fbd17fc87541621de81e0576e08d
Andrew Mazzola <admazzola@gmail.com>
2015-07-02 15:56:10 -0400

    added font files

--- JavaNESBrain/src/com/starflask/JavaNESBrain/data/BrainInfoWindow.java
+++ JavaNESBrain/src/com/starflask/JavaNESBrain/data/BrainInfoWindow.java
@@ -116,17 +116,17 @@ public class BrainInfoWindow  {
 		Font awtFont = new Font("Times New Roman", Font.BOLD, 24);
 		font = new TrueTypeFont(awtFont, false);
 	 
-		/*// load font from a .ttf file
+		// load font from a .ttf file
 		try {
-			InputStream inputStream	= ResourceLoader.getResourceAsStream("myfont.ttf");
+			InputStream inputStream	= ResourceLoader.getResourceAsStream("assets/CONSTAN.ttf");
 	 
 			Font awtFont2 = Font.createFont(Font.TRUETYPE_FONT, inputStream);
 			awtFont2 = awtFont2.deriveFont(24f); // set font size
-			font2 = new TrueTypeFont(awtFont2, false);
+			font = new TrueTypeFont(awtFont2, false);
 	 
 		} catch (Exception e) {
 			e.printStackTrace();
-		}	*/
+		}	
 		
 	}
 
